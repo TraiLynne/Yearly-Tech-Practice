867ba850526b7740cfe9d35d8a4dddf92ddf3a4b
Trai Lynne Compton <trailynne@gmail.com>
2024-07-10 20:45:34 +0000

    Complete Complexity - `evens`

--- 00_Complexity/complexity.rb
+++ 00_Complexity/complexity.rb
@@ -67,8 +67,8 @@ p index_of([6, 4, 7, 9, 7, 8, 2, 4, 3], 7) # 2
 #    [9, 83, 74] --> [74]
 #    [6, 4, 7, 9, 7, 8, 2, 4, 3] --> [6, 4, 8, 2, 4]
 
-# Time Complexity:
-# Auxiliary Space Complexity:
+# Time Complexity: O(N)
+# Auxiliary Space Complexity: O(N)
 
 def evens(arr)
   results = []
