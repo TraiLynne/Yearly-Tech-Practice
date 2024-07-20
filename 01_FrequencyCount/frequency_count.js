2d9fc5203b5b07a9a05a1958ad31b4cd02acb11e
Trai Lynne Compton <trailynne@gmail.com>
2024-07-20 12:25:16 -0600

    Complete JS FrequencyCount - `letterSort`

--- 01_FrequencyCount/frequency_count.js
+++ 01_FrequencyCount/frequency_count.js
@@ -209,7 +209,49 @@ function missingNumber(n, arr) {
  */
 
 function letterSort(string) {
-  // YOUR WORK HERE
+  // Variables
+  let tracker = {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0,
+    g: 0,
+    h: 0,
+    i: 0,
+    j: 0,
+    k: 0,
+    l: 0,
+    m: 0,
+    n: 0,
+    o: 0,
+    p: 0,
+    q: 0,
+    r: 0,
+    s: 0,
+    t: 0,
+    u: 0,
+    v: 0,
+    w: 0,
+    x: 0,
+    y: 0,
+    z: 0
+  }
+  let i = 0;
+  let result = '';
+
+  // Update Tracker 
+  for(i ; i < string.length ; i++){
+    tracker[string[i]]++;
+  };
+
+  // Create result 
+  for(let key in tracker){
+    result += key.repeat(tracker[key])
+  }
+
+  return result;
 }
 
 
